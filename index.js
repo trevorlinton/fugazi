ae077a1c46dbf39200049ef500eeee93e95d5681
Trevor Linton <trevor.linton+github@gmail.com>
2022-01-05 20:53:41 -0700

    ah come on

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("hello246");
+  res.write("hello2467");
   res.end();
 }).listen(process.env.PORT);
