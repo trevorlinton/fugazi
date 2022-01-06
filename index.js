0dbc39be390fe03ed249269bfd9cf4482d174360
Trevor Linton <trevor.linton+github@gmail.com>
2022-01-05 21:01:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("hello246437");
+  res.write("hello1");
   res.end();
 }).listen(process.env.PORT);
